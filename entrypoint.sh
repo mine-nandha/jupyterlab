a40d8a07c09d64049bd7ce1b45d39e7cc55340d7
javsubs91 <80193522+javsubs91@users.noreply.github.com>
2021-11-10 16:17:05 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,5 @@ wget -nc $CONFIG_FILE_IN_URL -O '/.config/rclone/rclone.conf'
 rclone version
 rclone version
 rclone listremotes > remotes.txt
-sudo vncserver :1 -geometry 1280x695 -economictranslate -dontdisconnect &
 service aria2 start
 jupyter lab --ip=0.0.0.0 --port=$PORT --ServerApp.token='' --ServerApp.password='' --allow-root
